63c4596155d65bc4f8c4e5207e471102bb9e463a
Davide Filosa <davideclaypool@gmail.com!>
2024-01-22 10:15:16 +0100

    created Login Form component

---
+++ components/auth/header.tsx
@@ -0,0 +1,25 @@
+import { Poppins } from "next/font/google";
+import { FaLock } from "react-icons/fa";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+interface HeaderProps {
+  label: string;
+}
+
+export const Header = ({ label }: HeaderProps) => {
+  return (
+    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="flex items-center gap-x-2">
+        <FaLock className="w-6 h-6" />
+        <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+      </div>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+};
